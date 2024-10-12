a2defc09aee9207cc97cc1cf3dfec19643ca56c3
Ishini Madakaladeniya <uppaleei@gmail.com>
2024-10-12 14:00:52 +0530

    changed port

--- app.js
+++ app.js
@@ -26,5 +26,5 @@ app.use(order);
 
 
 app.listen(process.env.PORT || 5000, () => {
-    console.log(`Server is running on port ${process.env.PORT || 5000}`);
+    console.log(`Server is running on port ${process.env.PORT}`);
 });
